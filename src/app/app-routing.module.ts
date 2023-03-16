465a8c8843a455848f64f58d709fa5ee631e0c3d
Ismael Dominguez <ismaeldb_@hotmail.com>
2023-03-16 04:35:07 -0500

    Actualiza el readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {path:'alarms/shutdown', component: ShutdownMethodComponent},
   {path:'calendar', component: CalendarComponent},
   {path:'profile', component: ProfileComponent},
-  {path:'login', component: LoginComponent},
+  {path:'', component: LoginComponent},
 ];
 
 @NgModule({
